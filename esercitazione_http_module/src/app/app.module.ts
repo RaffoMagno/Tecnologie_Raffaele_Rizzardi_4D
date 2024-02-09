33b3ba20e23d35d8127f8b6d1e31454569d9cbdb
Raffaele Rizzardi <rizzardi.raffaele@iisgalvanimi.edu.it>
2024-02-09 15:43:46 +0000

    svolto esercizio 15.2

--- esercitazione_http_module/src/app/app.module.ts
+++ esercitazione_http_module/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FooComponent } from './foo/foo.component'; //Importa Il modulo http
+
+import { HttpClientModule } from '@angular/common/http'; //Importa Il modulo http
+import { FooComponentComponent } from './foo/foo.component';
 
 @NgModule({
- declarations: [
-   AppComponent,
-   FooComponent
- ],
- imports: [
-   BrowserModule, HttpClientModule //Aggiungi il modulo http qui 
- ],
- providers: [],
- bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    FooComponentComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
